f2d7ba8bf41505d6127d7129e5731d54112dd728
Francis Molina <francisjmp99@gmail.com>
2024-10-24 21:07:20 +0200

    feat(App): add searchBar back

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,39 @@
+import { useState } from 'react';
 import './App.css';
 import { CardComponent } from './CardComponent';
 import { wildShapes } from './wildShapes';
 
 function App() {
+  const [searchValue, setSearchValue] = useState('');
+
+  const handleSearch = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchValue(event.target.value);
+  };
+
+  const SearchBar = () => {
+    return (
+      <input
+        className="searchBar"
+        autoFocus
+        type="text"
+        placeholder="Buscar..."
+        value={searchValue}
+        onChange={handleSearch}
+      />
+    );
+  };
+
+  const filteredWildShapes = wildShapes.filter((shape) => shape.name.toLowerCase().includes(searchValue.toLowerCase()));
+
   return (
-    <div className="App">
-      <div className="App-header">
-        <div className="cards">
-          {wildShapes.map((card) => (
-            <CardComponent card={card} />
-          ))}
-        </div>
+    <div className="layout">
+      <h1>Formas Salvajes</h1>
+      <SearchBar />
+      {filteredWildShapes.length === 0 && <p>No se encontraron resultados</p>}
+      <div className="cards">
+        {filteredWildShapes.map((card) => (
+          <CardComponent card={card} />
+        ))}
       </div>
     </div>
   );
