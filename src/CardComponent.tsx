c6f933ec97050ba123353bd2d8f3eef70cc9d1b7
Francis Molina <francisjmp99@gmail.com>
2024-10-24 20:15:32 +0200

    chore: remove unused method

--- src/CardComponent.tsx
+++ src/CardComponent.tsx
@@ -4,33 +4,6 @@ import { StatIndicator } from './StatIndicator';
 import { ChallengeRatingBadge } from './ChallengeRatingBadge';
 
 export const CardComponent = ({ card }: { card: WildShape }) => {
-  const getRatingBannerStyle = (rating: string) => {
-    if (rating === '0') {
-      return styles.banner0;
-    }
-    if (rating === '1/4') {
-      return styles.banner1_4;
-    }
-    if (rating === '1/2') {
-      return styles.banner1_2;
-    }
-    if (rating === '1') {
-      return styles.banner1;
-    }
-    if (rating === '2') {
-      return styles.banner2;
-    }
-    if (rating === '3') {
-      return styles.banner3;
-    }
-    if (rating === '4') {
-      return styles.banner4;
-    }
-    if (rating === '5') {
-      return styles.banner5;
-    }
-    return '';
-  };
   return (
     <div className={styles.card}>
       <img className={styles.backgroundImage} src={card.imageUrl} alt="Monster" />
